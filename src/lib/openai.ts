3d41981fa2c141887ba769db8deed8417ca8f02c
zjpiazza <zjpiazza@gmail.com>
2025-02-02 18:49:44 -0600

    Remove dummy values

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,10 +1,11 @@
 import OpenAI from 'openai';
 
 export const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY || 'dummy',
-  baseURL: process.env.OPENAI_API_BASE_URL || 'https://api.openai.com/v1',
+  apiKey: process.env.OPENAI_API_KEY,
+  baseURL: process.env.OPENAI_API_BASE_URL,
   defaultHeaders: {
-    'CF-Access-Client-Id': process.env.CF_ACCESS_CLIENT_ID || 'dummy',
-    'CF-Access-Client-Secret': process.env.CF_ACCESS_CLIENT_SECRET || 'dummy'
+    'CF-Access-Client-Id': process.env.CF_ACCESS_CLIENT_ID,
+    'CF-Access-Client-Secret': process.env.CF_ACCESS_CLIENT_SECRET
   }
+
 });
